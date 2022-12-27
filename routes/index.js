48c4708c64fd20cd3e2e317d474c7ee5a9efa9e1
raghav8197 <raghav8197@gmail.com>
2022-12-27 23:05:12 +0530

    after installing cors

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+});
+
 router.post('/post-data/', async(req,res) => {
   try {
     let details = await dataModel.create(req.body)
